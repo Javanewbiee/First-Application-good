b4889eab7e2556de01a019340dda2829eb6f166c
Javanewbiee <kkremerr@gmail.com>
2019-08-15 11:20:45 +0200

    Modul 24.2

---
+++ src/main/java/com/crud/tasks/config/CompanyConfig.java
@@ -0,0 +1,17 @@
+package com.crud.tasks.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class CompanyConfig {
+
+    @Value("$(info.app.company.name)")
+    private String companyName;
+    @Value("$(info.app.company.email)")
+    private String companyMail;
+    @Value("$(info.app.comapny.phone)")
+    private String companyPhone;
+}
